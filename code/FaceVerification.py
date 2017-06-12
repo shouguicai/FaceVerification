3a591c95fd6d4ffc7a9a7506977b26742efdc9ae
shouguicai <zhongyouguo@gmail.com>
2017-06-12 21:05:26 +0800

    add svm and fnn classifer

--- code/FaceVerification.py
+++ code/FaceVerification.py
@@ -41,7 +41,7 @@ def FaceVerification(X1,X2):
 
             # add path
             model='./models/20170512-110547.pb'
-            classifier_filename ='./models/my_classifier.pkl'
+            svm_classifier_filename ='./models/my_classifier.pkl'
 
             # Load the model
             facenet.load_model(model)
@@ -60,24 +60,43 @@ def FaceVerification(X1,X2):
             #print(images.shape)
             feed_dict = { images_placeholder:images, phase_train_placeholder:False }
             emb_array = sess.run(embeddings, feed_dict=feed_dict)
+            
+            mode = 'FNN'
 
-            classifier_filename_exp = os.path.expanduser(classifier_filename)
-            #print(embeddings.shape)
-            #print(emb_array.shape)
+            if (mode=='SVM'):
+                classifier_filename_exp = os.path.expanduser(svm_classifier_filename)
+                #print(embeddings.shape)
+                #print(emb_array.shape)
 
-            with open(classifier_filename_exp, 'rb') as infile:
-                (model, class_names) = pickle.load(infile)
+                with open(classifier_filename_exp, 'rb') as infile:
+                    (model, class_names) = pickle.load(infile)
 
-            embeddings1 = emb_array[0::2]  # x[startAt:endBefore:skip]
-            embeddings2 = emb_array[1::2]
+                embeddings1 = emb_array[0::2]  # x[startAt:endBefore:skip]
+                embeddings2 = emb_array[1::2]
 
-            predictions1 = model.predict_proba(embeddings1)
-            best_class_indices1 = np.argmax(predictions1, axis=1)
-            best_class_probabilities1 = predictions1[np.arange(len(best_class_indices1)), best_class_indices1]
+                prediction1 = model.predict_proba(embeddings1)
+                best_class_indices1 = np.argmax(prediction1, axis=1)
+                best_class_probabilities1 = prediction1[np.arange(len(best_class_indices1)), best_class_indices1]
 
-            predictions2 = model.predict_proba(embeddings2)
-            best_class_indices2 = np.argmax(predictions2, axis=1)
-            best_class_probabilities2 = predictions2[np.arange(len(best_class_indices2)), best_class_indices2]
+                prediction2 = model.predict_proba(embeddings2)
+                best_class_indices2 = np.argmax(prediction2, axis=1)
+                best_class_probabilities2 = prediction2[np.arange(len(best_class_indices2)), best_class_indices2]
 
-            Y = np.equal(best_class_indices1, best_class_indices2)
+                Y = np.equal(best_class_indices1, best_class_indices2)
+            elif (mode=='FNN'):
+                #load meta graph and restore weights
+                saver = tf.train.import_meta_graph('./models/sparse_models/20170612-204222/my-model-fc-sc0-3400.meta')
+                saver.restore(sess,tf.train.latest_checkpoint('./models/sparse_models/20170612-204222/'))
+ 
+                # Get input and output tensors
+                graph = tf.get_default_graph()
+                x = graph.get_tensor_by_name("input/x-input:0")
+                keep_prob = graph.get_tensor_by_name("dropout/keep_prob:0")
+                y_hat = graph.get_tensor_by_name("output/activation:0")
+                feed_dict = { x:emb_array,keep_prob:1.0 }
+                predictions = sess.run(y_hat, feed_dict=feed_dict)
+                best_class_indices1 = np.where(predictions[0]==np.max(predictions[0]))[0][0]
+                best_class_indices2 = np.where(predictions[1]==np.max(predictions[1]))[0][0]
+                Y = np.equal(best_class_indices1,best_class_indices2)
+
     return int(Y)
