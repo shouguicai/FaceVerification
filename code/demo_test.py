39b3cfaf99d70287dbefc1db8312051ddce1c772
shouguicai <zhongyouguo@gmail.com>
2017-06-13 11:02:03 +0800

    A demo for how to use FaceVerification function

--- code/demo_test.py
+++ code/demo_test.py
@@ -1,13 +1,14 @@
 '''
 	A demo for how to use FaceVerification function
+	figure must be jpeg format
 '''
 from __future__ import absolute_import
 from __future__ import print_function
 
 from scipy import misc
 from FaceVerification import FaceVerification
-path1 = './datasets/my_dataset/test/0000045/013.jpg'
-path2 = './datasets/my_dataset/test/0000045/015.jpg'
+path1 = 'path/to/fig1'
+path2 = 'path/to/fig2'
 x1 = misc.imread(path1)
 x2 = misc.imread(path2)
 y = FaceVerification(x1,x2)
